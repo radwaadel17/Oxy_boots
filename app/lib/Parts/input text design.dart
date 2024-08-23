8042b8d362f0e99f7a3d1a651d9fa811cb691ceb
radwaadel17 <radwaadel959@gmail.com>
2024-08-23 20:38:47 +0300

    Loading indicator

--- app/lib/Parts/input text design.dart
+++ app/lib/Parts/input text design.dart
@@ -1,41 +1,40 @@
 import 'package:flutter/material.dart';
 
 class TextFielddesign extends StatelessWidget {
-   TextFielddesign({super.key , required this.txt , required this.onChanged});
-  String txt ; 
-   Function(String)? onChanged;
+  TextFielddesign({super.key, required this.txt, required this.onChanged});
+  String txt;
+  Function(String)? onChanged;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-    
         Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Text(
-              '$txt',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Text(
+            '$txt',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Container(
-              padding: EdgeInsets.only(left: 10),
-              width: 335,
-              height: 48,
-             decoration: BoxDecoration(
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Container(
+            padding: EdgeInsets.only(left: 10),
+            width: 335,
+            height: 48,
+            decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(24),
-             ),
-             child: TextField(
-              onChanged: onChanged ,
+            ),
+            child: TextField(
+                    onChanged: onChanged,
               decoration: InputDecoration(
                 border: InputBorder.none,
                 contentPadding: EdgeInsets.zero,
               ),
-             ),
             ),
           ),
+        ),
       ],
     );
   }
--- app/lib/views/Sign up.dart
+++ app/lib/views/Sign up.dart
@@ -3,6 +3,7 @@ import 'package:app/Parts/input%20text%20design.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class signUp extends StatefulWidget {
   const signUp({super.key});
@@ -15,135 +16,147 @@ class _signUpState extends State<signUp> {
   String? name;
   String? email;
   String? Password;
+  bool  Isloading  = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xfff8f9fb),
-      body: ListView(
-        children: [
-          const SizedBox(
-            height: 60,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
+    return ModalProgressHUD(
+      inAsyncCall: Isloading,
+      child: Scaffold(
+        backgroundColor: const Color(0xfff8f9fb),
+        body: ListView(
+          children: [
+            const SizedBox(
+              height: 60,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  Text(
+                    'Create Account',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Let’s Create Account Together',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            TextFielddesign(
+              onChanged: (data) {
+                name = data;
+              },
+              txt: 'Your Name',
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextFielddesign(
+              onChanged: (data) {
+                email = data;
+              },
+              txt: 'Email Address',
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextFielddesign(
+              onChanged: (data) {
+                Password = data;
+              },
+              txt: 'Password',
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ButtomDesign(
+              label: 'Sign Up',
+              onTap: () async {
+                Isloading = true; 
+                setState(() {
+                  
+                });
+                try {
+                  await registerUser();
+                 showSnackbar(context, 'Success Registration');
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    showSnackbar(context, 'The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    showSnackbar(
+                        context, 'The account already exists for that email.');
+                  }
+                }
+                catch(e){
+                  showSnackbar(context, 'there was an error');
+                }
+                Isloading = false;
+                 setState(() {
+                  
+                });
+                //print(user.user!.displayName);
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 5),
+              child: Container(
+                  width: 335,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                          height: 20,
+                          child:
+                              Image(image: AssetImage('assets/Group 108.png'))),
+                      Text(
+                        ' Sign in with google',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Create Account',
+                  'Already have an account?',
                   style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
+                    color: Color(0xff707B81),
                   ),
                 ),
-                Text(
-                  'Let’s Create Account Together',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 16,
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    ' Sign in',
                   ),
                 )
               ],
-            ),
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          TextFielddesign(
-            onChanged: (data) {
-              name = data;
-            },
-            txt: 'Your Name',
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          TextFielddesign(
-            onChanged: (data) {
-              email = data;
-            },
-            txt: 'Email Address',
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          TextFielddesign(
-            onChanged: (data) {
-              Password = data;
-            },
-            txt: 'Password',
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          ButtomDesign(
-            label: 'Sign Up',
-            onTap: () async {
-              try {
-                await registerUser();
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  showSnackbar(context, 'The password provided is too weak.');
-                } else if (e.code == 'email-already-in-use') {
-                  showSnackbar(
-                      context, 'The account already exists for that email.');
-                }
-              }
-              catch(e){
-                showSnackbar(context, 'there was an error');
-              }
-              showSnackbar(context, 'Success Registration');
-              //print(user.user!.displayName);
-            },
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 5),
-            child: Container(
-                width: 335,
-                height: 48,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                        height: 20,
-                        child:
-                            Image(image: AssetImage('assets/Group 108.png'))),
-                    Text(
-                      ' Sign in with google',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                )),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Already have an account?',
-                style: TextStyle(
-                  color: Color(0xff707B81),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  ' Sign in',
-                ),
-              )
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
